f4ac421d4eb2e9f8986da607d1ed31d939d18c7a
lakshmanpalitha <lakshmanpalitha@gmail.com>
2016-03-13 22:47:00 +0530

    Home page tile banner added

--- skin/frontend/shopcrown/default/js/common.js
+++ skin/frontend/shopcrown/default/js/common.js
@@ -16,6 +16,51 @@ jQuery(document).ready(function(){
 		}
 		return false;
 		});
+
+
+
+
+
+    /* Tile Banner Section */
+    function tileBanner() {
+        var tileHeight, tileWidth, detailsHeight, detailsWidth, tile, details;
+        jQuery('.tile').mouseenter(function() {
+            tile = jQuery(this);
+            tileHeight=tile.outerHeight();
+            tileWidth=tile.outerWidth();
+            details=tile.find('.details');
+            detailsHeight=details.outerHeight();
+            detailsWidth=details.outerWidth();
+
+            tile.find('h2').stop().slideDown();
+            details.css('left', (tileWidth/2) - (detailsWidth/2));
+            details.stop().animate({
+                top: (tileHeight/2) - (detailsHeight/2),
+                opacity:1
+            }, 500,'easeOutQuart', function() {
+                // Animation complete.
+            });
+
+        })
+            .mouseleave(function() {
+                tile.find('h2').stop().slideUp();
+                details.stop().animate({
+                    top: 0 - detailsHeight,
+                    opacity:0
+                }, 250,'easeInQuart', function() {
+                    // Animation complete.
+                });
+
+
+            });
+
+    };
+    if(jQuery('.tile-banner').length){
+        tileBanner();
+    }
+
+
+
 	});
 	
 	
